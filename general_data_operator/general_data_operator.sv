da36e01e78550993ee831980b1a3ae5c67741a0e
Tauhoo <tauhoo_ice@hotmail.com>
2021-02-11 16:01:32 +0700

    create sigmoid and tanh activation function

--- general_data_operator/general_data_operator.sv
+++ general_data_operator/general_data_operator.sv
@@ -2,6 +2,14 @@
 module general_data_operator (out_data);
     import gdo::gdo_add;
 	import gdo::gdo_mult;
+    //import gdo::gdo_pow;
+    import gdo::gdo_div;
+    import gdo::gdo_sigmoid;
+    import gdo::gdo_tanh;
+    import gdo::gdo_binary;
+    import gdo::gdo_e;
+    import gdo::gdo_one;
+    import gdo::gdo_zero;
 
     output [16:0] out_data;
     reg [16:0] data;
@@ -10,9 +18,16 @@ module general_data_operator (out_data);
     initial begin
         data = 3;
         data = gdo_add(16, 16);
+        $write("in data : %d\n", (-1)*3*(2**8) << 1);
         $write("add : %d\n", gdo_add(16, 16));
         $write("mult : %d\n", gdo_mult(-128, 128));
-        // $write("sub : %d\n", gdo.sub(8'b0000_1000, 8'b0000_1000));
+        $write("sub : %d\n", gdo_div(1024, 768));
+        $write("sub : %d\n", gdo_div(1280, 768));
+        $write("sigmoid : %d %b\n", gdo_sigmoid((1)*3*(2**8)), (-1)*3*(2**8));
+        $write("sigmoid : %b\n", gdo_sigmoid(gdo_one >> 1));
+        $write("binary : %d\n", gdo_binary((1)*3*(2**8)));
+        $write("tanh : %d\n", gdo_tanh((1)*1*(2**8)));
+        $write("one : %d , zero : %d\n", gdo_one << 1, gdo_zero);
         // $write("pow : %d\n", gdo.pow(8'b0010_0000, 8'b0011_0000));
     end
 endmodule
--- general_data_operator/src/gdo.sv
+++ general_data_operator/src/gdo.sv
@@ -2,6 +2,9 @@
 package gdo;
     parameter gdo_size = 8;
     parameter gdo_data_size = 2*gdo_size;
+    parameter gdo_e = (271828 * (2**gdo_size))/(10**5);
+    const reg signed [gdo_data_size - 1:0] gdo_one = {{(gdo_size - 1){1'b0}},1'b1,{gdo_size{1'b0}}};
+    const reg signed [gdo_data_size - 1:0] gdo_zero = {gdo_data_size{1'b0}};
 
     function [gdo_data_size - 1:0] gdo_add;
         input reg signed [gdo_data_size - 1:0] a;
@@ -21,12 +24,60 @@ package gdo;
         end
     endfunction
 
+    function [gdo_data_size + gdo_size - 1:0] gdo_div;
+        input reg signed [gdo_data_size - 1:0] a;
+        input reg signed [gdo_data_size - 1:0] b;
+        reg signed [gdo_data_size + gdo_size - 1:0] ab;
+        begin
+            ab       = {a, {gdo_size{1'b0}}} / {{gdo_size{1'b0}}, b};
+            return ab[gdo_data_size - 1:0];
+        end
+    endfunction
+
     function [gdo_data_size - 1:0] gdo_pow;
         input reg signed [gdo_data_size - 1:0] a;
         input reg signed [gdo_data_size - 1:0] b;
         reg signed [gdo_data_size - 1:0] ab;
         begin
-            return $rtoi((a * 2.0 ** (-1.0*gdo_size))**(b * 2.0 ** (-1.0*gdo_size)) * 2.0 ** gdo_size);
+            return ((a / (2**gdo_size))**(b / (2**gdo_size)))*2**gdo_size;
+        end
+    endfunction
+
+    function [gdo_data_size - 1:0] gdo_sigmoid;
+        input reg signed [gdo_data_size - 1:0] x;
+        reg signed [gdo_data_size - 1:0] x_abs;
+        reg signed [gdo_data_size - 1:0] x_acc;
+        reg signed [gdo_data_size - 1:0] result;
+        begin
+            if (x >= 0) begin
+                x_abs = x;
+            end else begin
+                x_abs = -1 * x;
+            end
+            
+            result = gdo_one << 1;
+
+            x_acc = x_abs;
+            result = gdo_add(result, x_acc);
+
+            x_acc = gdo_mult(x_acc, x_abs) >> 1;
+            result = gdo_add(result, x_acc);
+
+            x_acc = gdo_mult(x_acc, x_abs) >> 2;
+            result = gdo_add(result, x_acc);
+
+            x_acc = gdo_mult(x_acc, x_abs) >> 1;
+            result = gdo_add(result, x_acc);
+
+            x_acc = gdo_mult(x_acc, x_abs) >> 3;
+            result = gdo_add(result, x_acc);
+
+            result = gdo_div(gdo_one ,result);
+            if (x > 0) begin
+                result = gdo_sub(gdo_one, result);
+            end 
+            
+            return result;
         end
     endfunction
 
@@ -35,6 +86,31 @@ package gdo;
         input reg signed [gdo_data_size - 1:0] b;
         begin
             return a - b;
+        end
+    endfunction
+
+    function [gdo_data_size - 1:0] gdo_linear;
+        input reg signed [gdo_data_size - 1:0] in_data;
+        begin
+            return in_data;
+        end
+    endfunction
+
+    function [gdo_data_size - 1:0] gdo_binary;
+        input reg signed [gdo_data_size - 1:0] in_data;
+        begin
+            if (in_data < 0) begin
+                return gdo_zero;
+            end else begin
+                return gdo_one;
+            end
+        end
+    endfunction
+
+    function [gdo_data_size - 1:0] gdo_tanh;
+        input reg signed [gdo_data_size - 1:0] in_data;
+        begin
+            return gdo_sub(gdo_sigmoid(in_data << 1) << 1, gdo_one);
         end
     endfunction
 
